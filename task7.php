9e1c59d428e20d751fd1e6e11fef9942a7a47f14
user <2l-u-b-o-v@ukr.net>
2017-07-15 12:57:33 +0300

    Исправлено: повторы <td> и лишнее уловие.

--- task7.php
+++ task7.php
@@ -6,17 +6,22 @@ $numbers = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
 echo "<table><tr>";
 for ($i = 1; $i <= 10; $i++) {
     for ($j = 1; $j <= 10; $j++) {
+        echo '<td>';
         if (($i % 2 == 0) and ($j % 2 == 0)) {
-            echo '<td>' . '(' . ($i * $j) . ')' . '</td>';
+           // echo '<td>' . '(' . ($i * $j) . ')' . '</td>';
+            echo '(' . ($i * $j) . ')' ;
         } elseif (($i % 2 != 0) and ($j % 2 != 0)) {
-            echo '<td>' . '[' . ($i * $j) . ']' . '</td>';
+            //echo '<td>' . '[' . ($i * $j) . ']' . '</td>';
+            echo '[' . ($i * $j) . ']';
         } else {
-            echo '<td>' . ($i * $j) . '</td>';
+           // echo '<td>' . ($i * $j) . '</td>';
+            echo ''. ($i * $j);
         }
+        echo '</td>';
     }
-    if ($i != 10) {
+   // if ($i != 10) {
         echo "</tr><tr>";
-    }
+ //   }
 }
 echo "</tr></<table>";
 ?>
