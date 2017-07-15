ab43c0c095abdc1f8aa593f853db9d09ece98fc4
user <2l-u-b-o-v@ukr.net>
2017-07-15 13:46:47 +0300

    Исправлено: пустая строка.

--- task7.php
+++ task7.php
@@ -3,8 +3,9 @@ echo '<style>';
 include "style.css";
 echo '</style>';
 $numbers = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
-echo "<table><tr>";
+echo "<table>";
 for ($i = 1; $i <= 10; $i++) {
+    echo '<tr>';
     for ($j = 1; $j <= 10; $j++) {
         echo '<td>';
         if (($i % 2 == 0) and ($j % 2 == 0)) {
@@ -20,7 +21,7 @@ for ($i = 1; $i <= 10; $i++) {
         echo '</td>';
     }
    // if ($i != 10) {
-        echo "</tr><tr>";
+        echo "</tr>";
  //   }
 }
 echo "</tr></<table>";
