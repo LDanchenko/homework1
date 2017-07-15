6bd1e8dc48f8128ab4f62a3aca48765512a11c06
user <2l-u-b-o-v@ukr.net>
2017-07-15 12:14:33 +0300

    Исправлено: дублирование  <td width = 80 align="center">, стиль кода.

--- task7.php
+++ task7.php
@@ -1,19 +1,22 @@
 <?php
+echo '<style>';
+include "style.css";
+echo '</style>';
 $numbers = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
 echo "<table><tr>";
-
-for ( $i = 1; $i <= 10; $i++) {
-
-    for ( $j =1; $j <=10 ; $j++)
-        if (($i%2==0) and ($j%2==0)){
-            echo  '<td width = 80 align="center">' . '('. ($i * $j) . ')'. '</td>'; }
-    else if (($i%2!=0) and ($j%2!=0)){
-        echo  '<td width = 80 align="center">' . '['. ($i * $j) . ']'. '</td>'; }
-    else echo  '<td width = 80 align="center">' .  ($i * $j) .  '</td>';
-            if ($i != 10) echo "</tr><tr>";
-
+for ($i = 1; $i <= 10; $i++) {
+    for ($j = 1; $j <= 10; $j++) {
+        if (($i % 2 == 0) and ($j % 2 == 0)) {
+            echo '<td>' . '(' . ($i * $j) . ')' . '</td>';
+        } elseif (($i % 2 != 0) and ($j % 2 != 0)) {
+            echo '<td>' . '[' . ($i * $j) . ']' . '</td>';
+        } else {
+            echo '<td>' . ($i * $j) . '</td>';
+        }
+    }
+    if ($i != 10) {
+        echo "</tr><tr>";
+    }
 }
 echo "</tr></<table>";
-
-
 ?>
