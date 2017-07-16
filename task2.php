1af564f96575176fd97e57738982d64387338870
user <2l-u-b-o-v@ukr.net>
2017-07-16 12:05:41 +0300

    Исправлено: удалены комментарии

--- task2.php
+++ task2.php
@@ -3,19 +3,16 @@ $drawing = 80;
 $markers = 23;
 $pencils = 40;
 $paints = 0;
-
-echo 'Всего рисунков = ' .$drawing;
-echo  '<br/>';
-echo 'Нарисовано фломастерами = ' .$markers;
-echo  '<br/>';
-echo 'Нарисовано каранадшами = ' .$pencils;
-echo  '<br/>';
+echo 'Всего рисунков = ' . $drawing;
+echo '<br/>';
+echo 'Нарисовано фломастерами = ' . $markers;
+echo '<br/>';
+echo 'Нарисовано каранадшами = ' . $pencils;
+echo '<br/>';
 echo 'Нарисовано красками = ?';
-echo  '<br/>';
-
+echo '<br/>';
 $paints = $drawing - $markers - $pencils;
-echo ' ' .$drawing. ' - ' .$markers . ' - ' . $pencils . ' = ' . $paints;
-echo  '<br/>';
-echo 'Ответ: ' .$paints . ' рисунков выполнили красками.';
-
+echo ' ' . $drawing . ' - ' . $markers . ' - ' . $pencils . ' = ' . $paints;
+echo '<br/>';
+echo 'Ответ: ' . $paints . ' рисунков выполнили красками.';
 ?>
