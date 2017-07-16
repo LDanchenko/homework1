d8c7d549fc3619a5bc59d1e185da8e4aac8b220a
user <2l-u-b-o-v@ukr.net>
2017-07-16 12:14:34 +0300

    Исправлено: убраны комментарии

--- task7.php
+++ task7.php
@@ -1,28 +1,20 @@
 <?php
-echo '<style>';
-include "style.css";
-echo '</style>';
 $numbers = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
 echo "<table>";
 for ($i = 1; $i <= 10; $i++) {
     echo '<tr>';
     for ($j = 1; $j <= 10; $j++) {
-        echo '<td>';
+        echo '<td width = 80 align="center" >';
         if (($i % 2 == 0) and ($j % 2 == 0)) {
-           // echo '<td>' . '(' . ($i * $j) . ')' . '</td>';
-            echo '(' . ($i * $j) . ')' ;
+            echo '(' . ($i * $j) . ')';
         } elseif (($i % 2 != 0) and ($j % 2 != 0)) {
-            //echo '<td>' . '[' . ($i * $j) . ']' . '</td>';
             echo '[' . ($i * $j) . ']';
         } else {
-           // echo '<td>' . ($i * $j) . '</td>';
-            echo ''. ($i * $j);
+            echo '' . ($i * $j);
         }
         echo '</td>';
     }
-   // if ($i != 10) {
-        echo "</tr>";
- //   }
+    echo "</tr>";
 }
 echo "</tr></<table>";
 ?>
